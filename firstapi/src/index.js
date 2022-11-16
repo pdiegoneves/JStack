801baf3f50b82cc7698b0e613dcb06e2668c128f
Diego Neves <pdcassiano@gmail.com>
2022-11-16 06:31:38 -0300

    urlparser

--- firstapi/src/index.js
+++ firstapi/src/index.js
@@ -1,27 +1,30 @@
 const http = require('http')
-const url = require('url')
+const { URL } = require('url')
 
 const routes = require('./routes')
 const server = http.createServer((request, response) => {
-    const parsedUrl = url.parse(request.url)
+  //   const parsedUrl = url.parse(request.url, true)
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`)
 
-    console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`)
+  console.log(
+    `Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`
+    )
 
-    const route = routes.find((routeObj) => (
-        routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
-    ))
+  const route = routes.find(
+    (routeObj) =>
+      routeObj.endpoint === parsedUrl.pathname &&
+      routeObj.method === request.method
+  )
 
-    console.log(route)
-
-    if (route) {
-        route.handler(request, response)
-    } else {
-        response.writeHead(404, { 'content-type': 'text/html'})
-        response.end(`Cannot ${request.method} ${parsedUrl.pathname}`)
-    }
-
+  if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams)
+    route.handler(request, response)
+  } else {
+    response.writeHead(404, { 'content-type': 'text/html' })
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`)
+  }
 })
 
 server.listen(3000, () => {
-    console.log('🔥 Server started at http://localhost:3000')
+  console.log('🔥 Server started at http://localhost:3000')
 })
